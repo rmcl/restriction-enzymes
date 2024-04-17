4a122b0d849be25ed7304b7ac2c13b046ea98d4a
Russell McLoughlin <russ.mcl@gmail.com>
2024-04-17 07:33:48 -0700

    add doc.go file.

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package restriction-enzymes is a Go package for working with restriction enzymes.
+
+This package contains the restriction enzymes from the REBASE database, as well as functions for working with restriction enzymes in general.
+*/
+package main
